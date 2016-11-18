a7c94e0520193e4e2f7c1158427948370e956a5b
alessiocoser <alessio.coser@xpeppers.com>
2016-11-18 14:19:11 +0100

    one short word does not wrap

--- word-wrap-test.js
+++ word-wrap-test.js
@@ -1,12 +1,25 @@
 var assert = require('assert')
 
 function wrap(text, length) {
-  return ''
+  if (text == null) {
+    return ''
+  }
+
+  return text
 }
 
 
 describe('Word Wrap', () => {
   it('wrap null returns empty string', () => {
-    assert.equal(wrap(null, 10), "")
+    assert.equal(wrap(null, 10), '')
   })
+
+  it('wrap empty string returns empty string', () => {
+    assert.equal(wrap('', 10), '')
+  })
+
+  it('one short word does not wrap', () => {
+    assert.equal(wrap('word', 10), 'word')
+  })
+
 })
