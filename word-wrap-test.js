4aa49384c7d53641cb66e7bfa260467540001210
alessiocoser <alessio.coser@xpeppers.com>
2016-11-19 15:54:11 +0100

    extracts wrap function to external module

--- word-wrap-test.js
+++ word-wrap-test.js
@@ -1,26 +1,5 @@
 var assert = require('assert')
-
-function wrap(text, length) {
-  if (length < 1) {
-    throw new Error('Invalid Argument');
-  }
-
-  if (text == null) {
-    return ''
-  }
-
-  if (text.length <= length) {
-    return text;
-  }
-
-  var space = text.indexOf(' ');
-  if (space >= 0) {
-    return 'word\nword';
-  }
-
-  return text.substring(0, length) + "\n" + wrap(text.substring(length), length)
-}
-
+var wrap = require('./word-wrap')
 
 describe('Word Wrap', () => {
   it('wrap null returns empty string', () => {
