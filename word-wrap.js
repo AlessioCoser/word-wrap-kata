132fed5ea218aa8f667b35eb80df93275be599d5
alessiocoser <alessio.coser@xpeppers.com>
2016-11-19 16:28:16 +0100

    refactor to remove duplication, use es6 class with static method

--- word-wrap.js
+++ word-wrap.js
@@ -1,21 +1,40 @@
-module.exports = function wrap(text, length) {
-  if (length < 1) {
-    throw new Error('Invalid Argument');
+class WordWrap {
+  constructor(length) {
+    this.length = length
   }
 
-  if (text == null) {
-    return ''
+  static wrap(text, length) {
+    return new WordWrap(length).wrap(text)
   }
 
-  if (text.length <= length) {
-    return text;
+  wrap(text) {
+    if (this.length < 1) {
+      throw new Error('Invalid Argument');
+    }
+
+    if (text == null) {
+      return ''
+    }
+
+    if (text.length <= this.length) {
+      return text;
+    }
+
+    var space = text.indexOf(' ')
+
+    if (space >= 0) {
+      return this.breakBetween(text, space, space+1)
+    }
+
+    return this.breakBetween(text, this.length, this.length)
   }
 
-  var space = text.indexOf(' ')
-
-  if (space >= 0) {
-    return text.substring(0, space) + "\n" + wrap(text.substring(space + 1), space + 1)
+  breakBetween(text, start, end) {
+    return text.substring(0, start)
+    + "\n"
+    + WordWrap.wrap(text.substring(end), this.length);
   }
-
-  return text.substring(0, length) + "\n" + wrap(text.substring(length), length)
 }
+
+
+module.exports = WordWrap.wrap
